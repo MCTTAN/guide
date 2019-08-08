51273d61573c1919a3e639145e031b8a25bc3aa2
M.C. Tan <mariechantaltan@gmail.com>
2019-08-08 08:07:03 -0400

    Create position_identity.R

---
+++ r_studio/chart_types/position_identity.R
@@ -0,0 +1,10 @@
+ggplot(
+  data = diamonds,
+  mapping = aes(x = cut, fill = clarity)
+) +
+  geom_bar(alpha = 1/5, position = "identity")
+ggplot(
+  data = diamonds,
+  mapping = aes(x = cut, color = clarity)
+) +
+  geom_bar(fill = NA, position = "identity")
