706944d3c1c8f9a7950c09d7465dcbf3f89e3899
M.C. Tan <mariechantaltan@gmail.com>
2019-08-08 08:25:27 -0400

    Create colors.R

---
+++ r_studio/chart_types/bar_charts/colors.R
@@ -0,0 +1,16 @@
+# Vector of n contiguous colors
+
+# Use rainbow colors
+barplot(1:5, col = rainbow(5))
+
+# Use heat colors
+barplot(1:5, col = heat.colors(5))
+
+# Use terrain colors
+barplot(1:5, col = terrain.colors(5))
+
+# Use topographic colors
+barplot(1:5, col = topo.colors(5))
+
+# Use tundra colors
+barplot(1:5, col = cm.colors(5))
