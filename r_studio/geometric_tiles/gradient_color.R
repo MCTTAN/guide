3a5c249f28fe7cc0009db9c51e61a88076dddb78
M.C. Tan <mariechantaltan@gmail.com>
2019-08-08 08:27:12 -0400

    Create gradient_color.R

---
+++ r_studio/geometric_tiles/gradient_color.R
@@ -0,0 +1,7 @@
+pal <- wes_palette("GrandBudapest1", 100, type = "continuous")
+ggplot(heatmap, aes(x = X2, y = X1, fill = value)) +
+  geom_tile() + 
+  scale_fill_gradientn(colours = pal) + 
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_discrete(expand = c(0, 0)) + 
+  coord_equal() 
